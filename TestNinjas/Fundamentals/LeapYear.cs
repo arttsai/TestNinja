41e2e1ce1313ed6392b6c997c66a3f78b8e26e4a
Art Tsai <arttsai@gmail.com>
2022-06-11 09:49:38 +0800

    fix .gitignore, comment leapyear.cs

--- TestNinjas/Fundamentals/LeapYear.cs
+++ TestNinjas/Fundamentals/LeapYear.cs
@@ -4,6 +4,8 @@ namespace TestNinja.Fundamentals
     {
         public bool IsLeapYear(int year)
         {
+            // rules:  https://keisan.casio.com/exec/system/1532572418
+            // another comment line 
             if (year % 4 != 0) return false;
             if (year % 100 != 0) return true;
             if (year % 400 != 0) return false;
